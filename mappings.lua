0a87abd1ad0105b77205909756302509f41d79a0
Ilkka Poutanen <ilkka@ilkka.dev>
2023-03-21 13:45:09 +0200

    Move hop mappings to new format

--- mappings.lua
+++ mappings.lua
@@ -23,6 +23,31 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    ['<leader><leader>f'] = {
+      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", desc =
+    "Hop forward to char" },
+    ['<leader><leader>F'] = {
+      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", desc =
+    "Hop backward to char" },
+    ['<leader><leader>w'] = {
+      "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.START })<cr>", desc =
+    "Hop to beginning of word" },
+    ['<leader><leader>e'] = {
+      "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END })<cr>", desc =
+    "Hop to end of word" },
+    ['<leader><leader>s'] = { "<cmd>HopPattern<cr>", desc = "Hop to pattern" },
+    ['<leader><leader>j'] = { "<cmd>HopLineAC<cr>", desc = "Hop to line below" },
+    ['<leader><leader>k'] = { "<cmd>HopLineBC<cr>", desc = "Hop to line above" },
+    ['<leader><leader>a'] = { "<cmd>HopAnywhere<cr>", desc = "Hop anywhere" },
+  },
+  v = {
+    ['<leader><leader>w'] = {
+      "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.START })<cr>", desc =
+    "Hop to beginning of word" },
+    ['<leader><leader>e'] = {
+      "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END })<cr>", desc =
+    "Hop to end of word" },
   },
   t = {
     -- setting a mapping to false will disable it
