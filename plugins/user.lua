b5dc937f816db44da6bd82fcb1653e12f9cbf1b0
Ilkka Poutanen <ilkka@ilkka.dev>
2023-03-21 10:29:21 +0200

    Add fugitive

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,27 +9,51 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-      {
-        "phaazon/hop.nvim",
-        event = "BufRead",
-        config = function ()
-          require'hop'.setup()
-          vim.api.nvim_set_keymap('n', '<leader><leader>f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
-          vim.api.nvim_set_keymap('n', '<leader><leader>F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
-          vim.api.nvim_set_keymap('o', '<leader><leader>f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<cr>", {})
-          vim.api.nvim_set_keymap('o', '<leader><leader>F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, inclusive_jump = true })<cr>", {})
-          vim.api.nvim_set_keymap('', '<leader><leader>t', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
-          vim.api.nvim_set_keymap('', '<leader><leader>T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
-          vim.api.nvim_set_keymap('n', '<leader><leader>w', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.START })<cr>", {})
-          vim.api.nvim_set_keymap('v', '<leader><leader>w', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.START })<cr>", {})
-          vim.api.nvim_set_keymap('o', '<leader><leader>w', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.START, inclusive_jump = true })<cr>", {})
-          vim.api.nvim_set_keymap('n', '<leader><leader>e', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END })<cr>", {})
-          vim.api.nvim_set_keymap('v', '<leader><leader>e', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END })<cr>", {})
-          vim.api.nvim_set_keymap('o', '<leader><leader>e', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, inclusive_jump = true })<cr>", {})
-          vim.api.nvim_set_keymap('n', '<leader><leader>s', "<cmd>HopPattern<cr>", {})
-          vim.api.nvim_set_keymap('n', '<leader><leader>j', "<cmd>HopLineAC<cr>", {})
-          vim.api.nvim_set_keymap('n', '<leader><leader>k', "<cmd>HopLineBC<cr>", {})
-          vim.api.nvim_set_keymap('n', '<leader><leader>a', "<cmd>HopAnywhere<cr>", {})
-        end
-      }
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require 'hop'.setup()
+      vim.api.nvim_set_keymap('n', '<leader><leader>f',
+        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
+        {})
+      vim.api.nvim_set_keymap('n', '<leader><leader>F',
+        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
+        {})
+      vim.api.nvim_set_keymap('o', '<leader><leader>f',
+        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<cr>",
+        {})
+      vim.api.nvim_set_keymap('o', '<leader><leader>F',
+        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, inclusive_jump = true })<cr>",
+        {})
+      vim.api.nvim_set_keymap('', '<leader><leader>t',
+        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
+        {})
+      vim.api.nvim_set_keymap('', '<leader><leader>T',
+        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
+        {})
+      vim.api.nvim_set_keymap('n', '<leader><leader>w',
+        "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.START })<cr>", {})
+      vim.api.nvim_set_keymap('v', '<leader><leader>w',
+        "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.START })<cr>", {})
+      vim.api.nvim_set_keymap('o', '<leader><leader>w',
+        "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.START, inclusive_jump = true })<cr>",
+        {})
+      vim.api.nvim_set_keymap('n', '<leader><leader>e',
+        "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END })<cr>", {})
+      vim.api.nvim_set_keymap('v', '<leader><leader>e',
+        "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END })<cr>", {})
+      vim.api.nvim_set_keymap('o', '<leader><leader>e',
+        "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, inclusive_jump = true })<cr>",
+        {})
+      vim.api.nvim_set_keymap('n', '<leader><leader>s', "<cmd>HopPattern<cr>", {})
+      vim.api.nvim_set_keymap('n', '<leader><leader>j', "<cmd>HopLineAC<cr>", {})
+      vim.api.nvim_set_keymap('n', '<leader><leader>k', "<cmd>HopLineBC<cr>", {})
+      vim.api.nvim_set_keymap('n', '<leader><leader>a', "<cmd>HopAnywhere<cr>", {})
+    end
+  },
+  {
+    'https://tpope.io/vim/fugitive.git',
+    lazy = false
+  },
 }
